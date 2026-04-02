c75c59e5d86f2e1a5bed5a4c0edec67af62f14d7
dzmitry-lahoda <dzmitry@lahoda.pro>
2026-04-02 19:33:44 -0300

    shortcut for structs

--- tests/docs/basics.rs
+++ tests/docs/basics.rs
@@ -1,2 +1,10 @@
 //! Primitive, simple structs, single depth, single item, single row, single array a like.
 //! One feature at a time. 
+
+
+#[test]
+fn doc() {
+    #[rowview::rows(root = Root)]
+    mod schema {
+
+    }
