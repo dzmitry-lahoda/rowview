65668e9c578f3dfa60cdbe82c2fab57684c0b287
dzmitry-lahoda <dzmitry@lahoda.pro>
2026-04-02 22:59:26 -0300

    more amd more scenarios, will AI survice?

--- tests/docs/advanced/incrementer.rs
+++ tests/docs/advanced/incrementer.rs
@@ -1,1 +1,44 @@
-//! By closure
+//! By closure or method
+
+#[test]
+fn increment_from_local_trait_method() {
+    trait Next {
+        fn next(self) -> Self;
+    }
+
+    impl Next for u32 {
+        fn next(self) -> Self {
+            self + 1
+        }
+    }
+
+    struct Root {
+        value: u32,
+        axis: Vec<(char, f32)>,
+    }
+
+    #[rowview::rows(root = Root)]
+    mod schema {
+        #[rowset(name = incremented, axis = root.axis)]
+        struct IncrementedRow {
+            #[copy(increment = root.value.next())]
+            counter: u32,
+            #[from_axis(axis.1)]
+            rest: f32,
+        }
+    }
+
+    let root = Root {
+        value: 41,
+        axis: vec![('a', 0.5), ('b', 111.111), ('c', 666.0)],
+    };
+    let incremented_rows = root.to_rows().incremented;
+
+    assert_eq!(incremented_rows.len(), 3);
+    assert_eq!(incremented_rows[0].counter, 42);
+    assert_eq!(incremented_rows[1].counter, 43);
+    assert_eq!(incremented_rows[2].counter, 44);
+    assert_eq!(incremented_rows[0].rest, 0.5);
+    assert_eq!(incremented_rows[1].rest, 111.111);
+    assert_eq!(incremented_rows[2].rest, 666.0);
+}
---
+++ tests/docs/basics/index.rs
@@ -0,0 +1,6 @@
+//! axis element position value
+
+#[test]
+fn axis_element_position_value() {
+    
+}
